65dc4ae3c3317c53cff21d5ddec835cf6cfef9cc
Adnan10101 <mohammedadnan10101@gmail.com>
2024-05-31 20:52:40 -0400

    updated flask files

--- Flask/app.py
+++ Flask/app.py
@@ -1,11 +1,22 @@
 from flask import Flask,render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
-def hello_world():
-    
-    return render_template('index.html')
+def home():
+    visuals_path = ["cloud cover.png",
+                    "humidity.png",
+                    "pressue.png",
+                    "rain.png",
+                    "snow.png",
+                    "temperature.png",
+                    "wind_speed.png"]
+    return render_template('home.html',images = visuals_path)
+
+@app.route("/forecast")
+def forecast():
+    return render_template('forecast.html')
 
 if __name__ == "__main__":
-    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',debug=True)
